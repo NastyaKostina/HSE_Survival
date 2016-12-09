6a5be2fa3c74102106f4e0dc66d4e63a7434ac2a
mark <mark@HOME-PC>
2016-12-10 01:16:19 +0300

    Двиения влево вправо и прыжок

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Ground = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer_move = new System.Windows.Forms.Timer(this.components);
+            this.Sky = new System.Windows.Forms.Panel();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.timer_Jump = new System.Windows.Forms.Timer(this.components);
+            this.timer_Falling = new System.Windows.Forms.Timer(this.components);
+            this.Sky.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ground
+            // 
+            this.Ground.BackColor = System.Drawing.Color.Lime;
+            this.Ground.Location = new System.Drawing.Point(0, 269);
+            this.Ground.Name = "Ground";
+            this.Ground.Size = new System.Drawing.Size(669, 69);
+            this.Ground.TabIndex = 1;
+            // 
+            // timer_move
+            // 
+            this.timer_move.Enabled = true;
+            this.timer_move.Interval = 1;
+            this.timer_move.Tick += new System.EventHandler(this.timer_move_Tick);
+            // 
+            // Sky
+            // 
+            this.Sky.BackColor = System.Drawing.Color.Cyan;
+            this.Sky.Controls.Add(this.Player);
+            this.Sky.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Sky.Location = new System.Drawing.Point(0, 0);
+            this.Sky.Name = "Sky";
+            this.Sky.Size = new System.Drawing.Size(669, 270);
+            this.Sky.TabIndex = 2;
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Player.Location = new System.Drawing.Point(46, 229);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(26, 41);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // timer_Jump
+            // 
+            this.timer_Jump.Enabled = true;
+            this.timer_Jump.Interval = 1;
+            this.timer_Jump.Tick += new System.EventHandler(this.timer_Jump_Tick);
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 340);
+            this.Controls.Add(this.Sky);
+            this.Controls.Add(this.Ground);
+            this.Name = "Form1";
+            this.Text = "Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.Sky.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel Ground;
+        private System.Windows.Forms.Timer timer_move;
+        public System.Windows.Forms.PictureBox Player;
+        public System.Windows.Forms.Panel Sky;
+        private System.Windows.Forms.Timer timer_Jump;
+        private System.Windows.Forms.Timer timer_Falling;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool Player_Left;
+        bool Player_Right;
+        int movement_speed = 5;
+        bool Player_Jump = false;
+        int jump_speed = 3;
+        int Force = 0;
+        int Gravity = 20;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    Player_Left = true;
+                    break;
+                case Keys.Right:
+                    Player_Right = true;
+                    break;
+                case Keys.Space:
+                    if (!Player_Jump && IsOutOfGame(Player))
+                    {
+                        Player_Jump = true;
+                        Force = Gravity;
+                        Player.Top -= jump_speed;
+                    }
+                    break;
+                case Keys.Escape:
+                    Close();
+                    break;
+
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    Player_Left = false;
+                    break;
+                case Keys.Right:
+                    Player_Right = false;
+                    break;
+            }
+        }
+
+        private void timer_move_Tick(object sender, EventArgs e)
+        {
+            if (Player_Right && Player.Right <= Sky.Width - 3)
+            {
+                Player.Left += movement_speed;
+            }
+            if (Player_Left && Player.Left >= 3)
+            {
+                Player.Left -= movement_speed;
+            }
+        }
+
+        public bool IsOutOfGame(PictureBox Player1)
+        {
+            if (Player1.Location.X < 0 || Player1.Location.X > Sky.Width)
+                return true; //out of width of the screen
+            if (Player1.Location.Y + Player1.Height > Sky.Height - 3 || Player1.Location.Y < 0)
+                return true; //out of top of the screen
+            return false;
+        }
+
+        private void timer_Jump_Tick(object sender, EventArgs e)
+        {
+            if (Force > 0)
+            {
+                if (IsOutOfGame(Player)) Force = 0;
+                else
+                {
+                    Force--;
+                    Player.Top -= jump_speed;
+                }
+            }
+            else
+                Player_Jump = false;
+
+            if (!Player_Jump && Player.Location.Y + Player.Height < Sky.Height - 3
+                && !IsOutOfGame(Player))
+                Player.Top += jump_speed;
+
+            if (!Player_Jump && Player.Location.Y + Player.Height > Sky.Height - 1)
+                Player.Top--;
+        }
+    
+    }
+}
