511698c2e5f4892f699951220fa44fd06f40feba
Костина Анастасия <nkd97@mail.ru>
2016-12-15 23:06:49 +0300

    Хотела как лучше, получилось как всегда

--- GameHSeSurvival/Player.cs
+++ GameHSeSurvival/Player.cs
@@ -13,14 +13,13 @@ namespace GameHSeSurvival
     
     class Player:Sprite
     {
-        public Vector2 move { get; set; }
-        private Vector2 oldPosition;
-        //public List<SoundEffect> sound_effects;
+        
         private int _Score;
-
+        PlayerMovementSystem Movements = new PlayerMovementSystem();
+        public Vector2 move;
         public int Score { get; set; }  
 
-        Vector2 changeposition;
+        //Vector2 changeposition;
 
         public Player(Texture2D texture, Vector2 position, SpriteBatch spritebatch) //List<SoundEffect> sounds)
         : base(texture, position, spritebatch)
@@ -29,86 +28,52 @@ namespace GameHSeSurvival
         }
         public void Update(GameTime gameTime)
         {
-            KeyboardAction();
-            GravityEffect();
-            MovingBlockChecking(); // ПРОВЕРКА ДВИГАЮЩИХСЯ БЛОКОВ
-            move -= move * new Vector2(.1f, .1f);
-            PossibleMoving(gameTime);
-            StopMovingIfBlocked();
-            IsPlayerUnderGround();
+            move = Movements.move;
+            Movements.Move(gameTime, this);
+            //MovingBlockChecking(); // ПРОВЕРКА ДВИГАЮЩИХСЯ БЛОКОВ
+            //IsPlayerUnderGround();
         }
-        private void KeyboardAction() //movements
-        {
-            KeyboardState keyboardState = Keyboard.GetState();
-            if (keyboardState.IsKeyDown(Keys.Left)) { move += -Vector2.UnitX * 0.5f; }
-            if (keyboardState.IsKeyDown(Keys.Right)) { move += Vector2.UnitX * 0.5f; }
-            if (keyboardState.IsKeyDown(Keys.Space) && IsOnFirmGround()) { move = -Vector2.UnitY * 32;/* sound_effects[0].Play();  подняли его вверх */ }
-        }
-        private void GravityEffect()
-        {
-            move += Vector2.UnitY * .65f;
-        }
-        private void UpdatePosition(GameTime gameTime)
-        {
-            Sprite_vector += move * (float)gameTime.ElapsedGameTime.TotalMilliseconds / 15;
-        }
-        private void PossibleMoving(GameTime gameTime)
-        {
-            oldPosition = Sprite_vector;
-            UpdatePosition(gameTime);
-            Sprite_vector = Board.CurrentBoard.WhereCanIGetTo(oldPosition, Sprite_vector, rectangle);
-        }
-        public bool IsOnFirmGround() //проверяем что стоим на земле
-        {
-            Rectangle onePixelLower = rectangle;
-            onePixelLower.Offset(0, 1);
-            return !Board.CurrentBoard.HasSpaceForRectangle(onePixelLower);
-        }
-        private bool IsMovingBlockOnTheRightSide()
-        {
-            Rectangle onePixelRighter = rectangle;
-            onePixelRighter.Offset(1, 0);
-            return !Board.CurrentBoard.HasSpaceToJumpFromTheLeftSide(this, onePixelRighter);
-        } // НОВЫЙ МЕТОД
-        private bool IsMovingBlockOnTheLeftSide()
-        {
-            Rectangle onePixelLefter = rectangle;
-            onePixelLefter.Offset(-1, 0);
-            return !Board.CurrentBoard.HasSpaceToJumpFromTheRightSide(this, onePixelLefter);
-        } // НОВЫЙ МЕТОД
-        private void MovingBlockChecking()
-        {
-            if (IsMovingBlockOnTheRightSide()) { Sprite_vector = new Vector2(Sprite_vector.X - 2, Sprite_vector.Y); }
-            if (IsMovingBlockOnTheLeftSide()) { Sprite_vector = new Vector2(Sprite_vector.X + 4, Sprite_vector.Y); }
-            Rectangle onePixelDown = rectangle;
-            Rectangle onePixelRight = rectangle;
-            onePixelDown.Offset(0, 1);
-            onePixelRight.Offset(1, 0);
-            bool[] whatToDo = Board.CurrentBoard.StandingOnTheMovingPlatform(this, ref changeposition, onePixelDown, onePixelRight);
-            if (whatToDo[0] && !whatToDo[1])
-            {
-                if (changeposition.X < 0)
-                    Sprite_vector = new Vector2(Sprite_vector.X - 2, Sprite_vector.Y);
-                else
-                    Sprite_vector = new Vector2(Sprite_vector.X + 2, Sprite_vector.Y);
-            }
-            if (whatToDo[0] && whatToDo[1])
-            {
-                Sprite_vector = new Vector2(Sprite_vector.X - 1, Sprite_vector.Y);
-            }
-        } // НОВЫЙ МЕТОД
-        private void StopMovingIfBlocked()
-        {
-            Vector2 lastMovement = Sprite_vector - oldPosition;
-            if (lastMovement.X == 0) { move *= Vector2.UnitY; }
-            if (lastMovement.Y == 0) { move *= Vector2.UnitX; }
-        }
-        private void IsPlayerUnderGround()
-        {
-            if (Sprite_vector.Y >= Board.CurrentBoard.rows * Board.CurrentBoard.block_texture.Height)
-            {
-                Sprite_vector = new Vector2(550, 576 - Sprite_texture.Height);
-            }
-        }
+        
+        //private bool IsMovingBlockOnTheRightSide()
+        //{
+        //    Rectangle onePixelRighter = rectangle;
+        //    onePixelRighter.Offset(1, 0);
+        //    return !Board.CurrentBoard.HasSpaceToJumpFromTheLeftSide(this, onePixelRighter);
+        //} // НОВЫЙ МЕТОД
+        //private bool IsMovingBlockOnTheLeftSide()
+        //{
+        //    Rectangle onePixelLefter = rectangle;
+        //    onePixelLefter.Offset(-1, 0);
+        //    return !Board.CurrentBoard.HasSpaceToJumpFromTheRightSide(this, onePixelLefter);
+        //} // НОВЫЙ МЕТОД
+        //private void MovingBlockChecking()
+        //{
+        //    if (IsMovingBlockOnTheRightSide()) { Sprite_vector = new Vector2(Sprite_vector.X - 2, Sprite_vector.Y); }
+        //    if (IsMovingBlockOnTheLeftSide()) { Sprite_vector = new Vector2(Sprite_vector.X + 4, Sprite_vector.Y); }
+        //    Rectangle onePixelDown = rectangle;
+        //    Rectangle onePixelRight = rectangle;
+        //    onePixelDown.Offset(0, 1);
+        //    onePixelRight.Offset(1, 0);
+        //    bool[] whatToDo = Board.CurrentBoard.StandingOnTheMovingPlatform(this, ref changeposition, onePixelDown, onePixelRight);
+        //    if (whatToDo[0] && !whatToDo[1])
+        //    {
+        //        if (changeposition.X < 0)
+        //            Sprite_vector = new Vector2(Sprite_vector.X - 2, Sprite_vector.Y);
+        //        else
+        //            Sprite_vector = new Vector2(Sprite_vector.X + 2, Sprite_vector.Y);
+        //    }
+        //    if (whatToDo[0] && whatToDo[1])
+        //    {
+        //        Sprite_vector = new Vector2(Sprite_vector.X - 1, Sprite_vector.Y);
+        //    }
+        //} // НОВЫЙ МЕТОД
+        
+        //private void IsPlayerUnderGround()
+        //{
+        //    if (Sprite_vector.Y >= Board.CurrentBoard.rows * Board.CurrentBoard.block_texture.Height)
+        //    {
+        //        Sprite_vector = new Vector2(550, 576 - Sprite_texture.Height);
+        //    }
+        //}
     }
 }
--- GameHSeSurvival/PlayerMovementSystem.cs
+++ GameHSeSurvival/PlayerMovementSystem.cs
@@ -11,28 +11,30 @@ namespace GameHSeSurvival
     class PlayerMovementSystem
     {
         
-        public Vector2 move { get; set; }
+        public  Vector2 move { get; set; }
         private Vector2 oldPosition;
+        Vector2 changeposition;
 
-        public void Move(GameTime gameTime, Vector2 Sprite_vector, Rectangle rectangle)
+        public void Move(GameTime gameTime, Player player)
         {
-            
-            KeyboardAction(rectangle);
+            KeyboardAction(player);
             GravityEffect();
             move -= move * new Vector2(.1f, .1f);
-            PossibleMoving(gameTime, Sprite_vector, rectangle);
-            StopMovingIfBlocked(Sprite_vector);
+            PossibleMoving(gameTime, player);
+            StopMovingIfBlocked(player);
+            MovingBlockChecking(player); // ПРОВЕРКА ДВИГАЮЩИХСЯ БЛОКОВ
+            IsPlayerUnderGround(player);
         }
-        private void KeyboardAction(Rectangle rectangle) //movements
+        private void KeyboardAction(Player player) //movements
         {
             KeyboardState keyboardState = Keyboard.GetState();
             if (keyboardState.IsKeyDown(Keys.Left)) { move += -Vector2.UnitX * 0.5f; }
             if (keyboardState.IsKeyDown(Keys.Right)) { move += Vector2.UnitX * 0.5f; }
-            if (keyboardState.IsKeyDown(Keys.Space) && IsOnFirmGround(rectangle)) { move = -Vector2.UnitY * 32;/*подняли его вверх */ }
+            if (keyboardState.IsKeyDown(Keys.Space) && IsOnFirmGround(player)) { move = -Vector2.UnitY * 32;/*подняли его вверх */ }
         }
-        private bool IsOnFirmGround(Rectangle rectangle) //проверяем что стоим на земле
+        private bool IsOnFirmGround(Player player) //проверяем что стоим на земле
         {
-            Rectangle onePixelLower = rectangle;
+            Rectangle onePixelLower = player.rectangle;
             onePixelLower.Offset(0, 1);
             return !Board.CurrentBoard.HasSpaceForRectangle(onePixelLower);
         }
@@ -40,21 +42,62 @@ namespace GameHSeSurvival
         {
             move += Vector2.UnitY * .65f;
         }
-        private void PossibleMoving(GameTime gameTime, Vector2 Sprite_vector, Rectangle rectangle)
+        private void PossibleMoving(GameTime gameTime, Player player)
         {
-            oldPosition = Sprite_vector;
-            UpdatePosition(gameTime, Sprite_vector);
-            Sprite_vector = Board.CurrentBoard.WhereCanIGetTo(oldPosition, Sprite_vector, rectangle);
+            oldPosition = player.Sprite_vector;
+            UpdatePosition(gameTime, player);
+            player.Sprite_vector = Board.CurrentBoard.WhereCanIGetTo(oldPosition, player.Sprite_vector, player.rectangle);
         }
-        private void UpdatePosition(GameTime gameTime, Vector2 Sprite_vector)
+        private void UpdatePosition(GameTime gameTime, Player player)
         {
-            Sprite_vector += move * (float)gameTime.ElapsedGameTime.TotalMilliseconds / 15;
+            player.Sprite_vector += move * (float)gameTime.ElapsedGameTime.TotalMilliseconds / 15;
         }
-        private void StopMovingIfBlocked(Vector2 Sprite_vector)
+        private void StopMovingIfBlocked(Player player)
         {
-            Vector2 lastMovement = Sprite_vector - oldPosition;
+            Vector2 lastMovement = player.Sprite_vector - oldPosition;
             if (lastMovement.X == 0) { move *= Vector2.UnitY; }
             if (lastMovement.Y == 0) { move *= Vector2.UnitX; }
+        }
+
+        private void MovingBlockChecking(Player player)
+        {
+            if (IsMovingBlockOnTheRightSide(player)) { player.Sprite_vector = new Vector2(player.Sprite_vector.X - 2, player.Sprite_vector.Y); }
+            if (IsMovingBlockOnTheLeftSide(player)) { player.Sprite_vector = new Vector2(player.Sprite_vector.X + 4, player.Sprite_vector.Y); }
+            Rectangle onePixelDown = player.rectangle;
+            Rectangle onePixelRight = player.rectangle;
+            onePixelDown.Offset(0, 1);
+            onePixelRight.Offset(1, 0);
+            bool[] whatToDo = Board.CurrentBoard.StandingOnTheMovingPlatform(player, ref changeposition, onePixelDown, onePixelRight);
+            if (whatToDo[0] && !whatToDo[1])
+            {
+                if (changeposition.X < 0)
+                    player.Sprite_vector = new Vector2(player.Sprite_vector.X - 2, player.Sprite_vector.Y);
+                else
+                    player.Sprite_vector = new Vector2(player.Sprite_vector.X + 2, player.Sprite_vector.Y);
+            }
+            if (whatToDo[0] && whatToDo[1])
+            {
+                player.Sprite_vector = new Vector2(player.Sprite_vector.X - 1, player.Sprite_vector.Y);
+            }
+        }
+        private bool IsMovingBlockOnTheRightSide(Player player)
+        {
+            Rectangle onePixelRighter = player.rectangle;
+            onePixelRighter.Offset(1, 0);
+            return !Board.CurrentBoard.HasSpaceToJumpFromTheLeftSide(player, onePixelRighter);
+        } // НОВЫЙ МЕТОД
+        private bool IsMovingBlockOnTheLeftSide(Player player)
+        {
+            Rectangle onePixelLefter = player.rectangle;
+            onePixelLefter.Offset(-1, 0);
+            return !Board.CurrentBoard.HasSpaceToJumpFromTheRightSide(player, onePixelLefter);
+        }
+        private void IsPlayerUnderGround(Player player)
+        {
+            if (player.Sprite_vector.Y >= Board.CurrentBoard.rows * Board.CurrentBoard.block_texture.Height)
+            {
+                player.Sprite_vector = new Vector2(550, 576 - player.Sprite_texture.Height);
+            }
         }
     }
 }
